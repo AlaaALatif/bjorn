3dd7f0a53fef64adabc65dab0a65def822c3c520
AlaaALatif <al.a.latif.94@gmail.com>
2020-12-07 15:55:17 -0800

    added functions to infer gene from gff and nucleotide position

--- bjorn_support.py
+++ bjorn_support.py
@@ -25,6 +25,66 @@ def get_variant_data(variant_filepaths: dict):
     return df
 
 
+def add_gene_column(df: pd.DataFrame) -> pd.DataFrame:
+    """Takes dataframe containing intra-host variant information and adds a column indicating the gene for each mutation
+     Uses the GFF_FEATURE to infer gene, and when GFF_FEATURE is missing (i.e. for indels), it uses nucleotide position 
+     to infer gene. The `gff2gene` mapping was obtained from the file /home/al/data/hcov19/gff/NC_045512.2.gff3 and using 
+     the create_gff2gene_mapping() function defined below."""
+    gff2gene = {
+        'cds-YP_009724389.1': 'ORF1ab',
+        'cds-YP_009725295.1': 'ORF1ab',
+        'cds-YP_009724390.1': 'S',
+        'cds-YP_009724396.1': 'ORF8',
+        'cds-YP_009724397.2': 'N',
+        'cds-YP_009724395.1': 'ORF7a',
+        'cds-YP_009724391.1': 'ORF3a',
+        'cds-YP_009724393.1': 'M',
+        'cds-YP_009724394.1': 'ORF6',
+        'cds-YP_009725255.1': 'ORF10',
+        'cds-YP_009725318.1': 'ORF7b',
+        'cds-YP_009724392.1': 'E'
+    }
+    # infer the gene from GFF_FEATURE
+    try:
+        df['gene'] = df['GFF_FEATURE'].apply(lambda x: gff2gene.get(x, 'nan'))
+    except:
+        raise KeyError('GFF_FEATURE column not found in the input dataframe.')
+    # infer the gene from position when GFF_FEATURE is missing
+    try:
+        df.loc[df['gene']=='nan', 'gene'] = df.loc[df['gene']=='nan', 'POS'].apply(map_gene_to_pos)
+    except:
+        raise KeyError('POS column not found in the input dataframe.')
+    return df
+    
+
+def map_gene_to_pos(x):
+    """helper function to infer the gene based on nucleotide position of SARS-CoV-2 genome"""
+    pos = x
+    if pos >= 269 and pos <= 21555:
+        return 'ORF1ab'
+    elif pos >= 21564 and pos <= 25382:
+        return 'S'
+    elif pos >= 25410 and pos <= 26214:
+        return 'ORF3a'
+    elif pos >= 26247 and pos <= 26471:
+        return 'E'
+    elif pos >= 26523 and pos <= 27187:
+        return 'M'
+    elif pos >= 27203 and pos <= 27382:
+        return 'ORF6'
+    elif pos >= 27398 and pos <= 27754:
+        return 'ORF7a'
+    elif pos >= 27757 and pos <= 27887:
+        return 'ORF7b'
+    elif pos >= 27896 and pos <= 28257:
+        return 'ORF8'
+    elif pos >= 28289 and pos <= 29528:
+        return 'N'
+    elif pos >= 29564 and pos <= 29670:
+        return 'ORF10'
+    return 'nan'
+
+
 def concat_fasta(in_dir, out_dir):
     """Concatenate fasta sequences into single fasta file"""
     cat_cmd = f"cat {in_dir}/*.fa > {out_dir}.fa"
@@ -34,3 +94,14 @@ def align_fasta(fasta_filepath):
     """Generate Multiple Sequence Alignment of concatenated sequences in input fasta file"""
     msa_cmd = f"mafft --auto --thread {num_cpus} {fasta_filepath}.fa > {fasta_filepath}_aligned.fa"
     return subprocess.check_call(msa_cmd, shell=True)
+
+
+def create_gff2gene_mapping(variant_data: pd.DataFrame, gff_filepath: str) -> dict:
+    fn = gffutils.example_filename(gff_filepath)
+    db = gffutils.create_db(fn, dbfn='test.db', merge_strategy='merge', force=True)
+    all_gffs = variant_data['GFF_FEATURE'].dropna().unique()
+    gff2gene = {}
+    for gff in all_gffs:
+        if gff:
+            gff2gene[gff] = db[gff]['gene'][0]
+    return gff2gene
