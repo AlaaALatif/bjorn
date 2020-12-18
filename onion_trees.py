eab0a09bc35a5ddc5b62158ed016f7654e35e025
AlaaALatif <al.a.latif.94@gmail.com>
2020-12-18 13:24:21 -0800

    visualize trees with indels annotated

--- onion_trees.py
+++ onion_trees.py
@@ -9,12 +9,13 @@ import numpy as np
 from Bio import SeqIO, AlignIO, Phylo, Align
 from itertools import groupby
 import more_itertools as mit
-from bjorn_support import map_gene_to_pos
+from bjorn_support import get_variant_filepaths, get_variant_data, map_gene_to_pos
 
 
-def visualize_tree(tree, sample_colors={}, figsize=(40, 100)):
-    f = plt.figure(figsize=figsize)
-    f = plt.figure()
+def visualize_tree(tree, sample_colors={}, 
+                   indels=None, colors=None, 
+                   isnv_info=False, figsize=(40, 100)):
+    fig = plt.figure(figsize=figsize)
     gs = gridspec.GridSpec(2, 2, width_ratios=[1,0.75], 
                            height_ratios = [0.5,0.5])
 
@@ -34,6 +35,18 @@ def visualize_tree(tree, sample_colors={}, figsize=(40, 100)):
         "c": [],
         "o": [],
     }
+    # add legends
+    if sample_colors:
+        indel2color = get_indel2color(indels, colors)
+        legend = get_color_legends(indel2color)
+        indel_legend = ax.legend(handles=legend)
+        l = plt.gca().add_artist(indel_legend)
+        if isnv_info:
+            sample_colors, freq2opacity = identify_isnv_indels(indels, tree, 
+                                                               sample_colors, colors)
+            isnv_legend = get_opacity_legends(freq2opacity)
+            ax.legend(handles=isnv_legend, loc='upper left', 
+                      title='iSNV freq')
     # for each sample
     for i in tree.get_terminals():
         _["x"].append(i.x)  # x coordinates
@@ -46,10 +59,76 @@ def visualize_tree(tree, sample_colors={}, figsize=(40, 100)):
     # draw annotated nodes
     ax.scatter(_["x"], _["y"], c = _["co"], s = 100, zorder = 2)
     ax.scatter(_["x"], _["y"], c = _["co"], s = 50, zorder = 2)
-    ax.legend(handles=legend)
-    f.patch.set_visible(False)
+        
+    fig.patch.set_visible(False)
     ax.axis('off')
-    return plt
+    return fig
+
+
+def identify_isnv_indels(indels, tree, sample_colors, colors):
+    indel_positions = get_indel_positions(indels['absolute_coords'])
+    non_indel_ids = get_non_indel_ids(tree, sample_colors)
+    variant_filepaths = get_variant_filepaths(non_indel_ids)
+    isnvs = get_variant_data(variant_filepaths)
+    isnvs = (isnvs.loc[(isnvs['ALT'].str.contains('-'))
+                    &(isnvs['POS'].isin(indel_positions))
+                    &(isnvs['sample'].isin(non_indel_ids)
+                    &(isnvs['ALT_FREQ']>0.03))]
+            [['sample', 'location', 'POS', 'REF', 'ALT', 'ALT_FREQ']])
+    del_idxs = isnvs.groupby('sample')['ALT_FREQ'].transform(max) == isnvs['ALT_FREQ']
+    isnvs = isnvs[del_idxs].drop_duplicates(subset=['sample'])
+    freq2opacity = create_freq2opacity(7)
+    indel2color = get_indel2color(indels, colors)
+    sample_colors = get_sample2color(indels, colors)
+    sample_colors = integrate_isnv_info(isnvs, sample_colors, 
+                                        indel2color, freq2opacity)
+    return sample_colors, freq2opacity
+
+
+def get_indel_positions(indel_coords: pd.Series) -> list:
+    return (indel_coords.apply(lambda x: int(x.split(':')[0]))
+                      .tolist())
+
+
+def get_non_indel_ids(tree, sample_colors):
+    sample_ids = [eval(n.name[n.name.find('SEARCH'):].split('-')[1]) for n in tree.get_terminals() if len(n.name.split('-')) >= 3]
+    indel_ids = [eval(s[s.find('SEARCH'):].split('-')[1]) for s in sample_colors.keys()]
+    non_indel_ids = set(sample_ids) - set(indel_ids)
+    return list(non_indel_ids)
+
+
+def create_freq2opacity(n: int):
+    freq2opacity = {}
+    for i in range(n):
+        freq2opacity[i] = (1./n)*(i+1)
+    return freq2opacity
+
+def map_opacity(freq: float, freq2opacity: dict):
+    x = int(freq*10)
+    return freq2opacity[x]
+
+
+def find_indel_coords(indel_start, indel2color):
+    for indel_coords in indel2color.keys():
+        if int(indel_coords.split(':')[0]) == indel_start:
+            return indel_coords
+
+
+def integrate_isnv_info(isnvs: pd.DataFrame, sample_colors: dict,
+                        indel2color: dict, freq2opacity: dict) -> dict:
+    """Update `sample_colors` with samples that contain any of the indels 
+    as minor variants"""
+    for i, td in enumerate(isnvs.iterrows()):
+        indel_start = td[1]['POS']
+        indel_coords = find_indel_coords(indel_start, indel2color)
+        sample_id = td[1]['sample']
+        freq = td[1]['ALT_FREQ']
+        loc = td[1]['location']
+        if indel_coords in indel2color.keys():
+            sample_name = f'SEARCH-{sample_id}-{loc}'
+            sample_colors[sample_name] = {'color': indel2color[indel_coords],
+                                     'freq': map_opacity(freq, freq2opacity)}
+    return sample_colors
 
 
 def load_tree(tree_path: str, patient_zero: str):
@@ -57,7 +136,28 @@ def load_tree(tree_path: str, patient_zero: str):
     `patient_zero` is a user-specified reference name for rooting the tree"""
     tree = next(Phylo.parse(tree_path, 'newick'))
     tree.root_with_outgroup(patient_zero)
-    return tree
+    return get_tree_coords(tree)
+
+
+def get_sample2color(deletions: pd.DataFrame, colors: list) -> dict:
+    deletions.sort_values('num_samples', inplace=True)
+    sample_colors = {}
+    indel2color = get_indel2color(deletions, colors)
+    for i, td in enumerate(deletions.iterrows()):
+        deletion = td[1]['absolute_coords']
+        samples = td[1]['samples']
+        for sample in samples:
+            sample_colors[sample] = {'color': indel2color[deletion],
+                                     'freq': 1.}
+    return sample_colors
+
+
+def get_indel2color(indels: pd.DataFrame, colors: list) -> dict:
+    indel2color = {}
+    for i, td in enumerate(indels.iterrows()):
+        indel = td[1]['absolute_coords']
+        indel2color[indel] = colors[i]
+    return indel2color
 
 
 def generate_color_array(colors, opacities):
@@ -113,7 +213,7 @@ def find_indel_coords(indel_start, indel2color):
             return indel_coords
 
 
-def get_coords(tree):
+def get_tree_coords(tree):
     """Takes Phylo tree object and populates it with coordinates 
     that can be used to plot the tree from scratch"""
     for _i, i in enumerate(tree.get_terminals()):
@@ -150,7 +250,7 @@ def identify_deletions(input_filepath: str, patient_zero: str, min_del_len: int=
     # only consider deletions longer than 2nts
     del_seqs = del_seqs[del_seqs['del_len'] > min_del_len]
     # fetch coordinates of each deletion
-    del_seqs['relative_coords'] = del_seqs['del_positions'].apply(get_coords)
+    del_seqs['relative_coords'] = del_seqs['del_positions'].apply(get_indel_coords)
     # group sample by the deletion they share
     del_seqs = (del_seqs.groupby(['relative_coords', 'del_len'])
                         .agg(samples=('idx', 'unique'),       # list of sample IDs with the deletion
@@ -193,7 +293,7 @@ def identify_insertions(input_filepath: str, patient_zero: str, min_ins_len: int
     # only consider deletions longer than 2nts
     ins_seqs = ins_seqs[ins_seqs['ins_len'] > min_ins_len]
     # fetch coordinates of each deletion
-    ins_seqs['relative_coords'] = ins_seqs['ins_positions'].apply(get_coords)
+    ins_seqs['relative_coords'] = ins_seqs['ins_positions'].apply(get_indel_coords)
     # group sample by the deletion they share
     ins_seqs = (ins_seqs.groupby(['relative_coords', 'ins_len'])
                         .agg(samples=('idx', 'unique'),       # list of sample IDs with the deletion
@@ -210,6 +310,7 @@ def identify_insertions(input_filepath: str, patient_zero: str, min_ins_len: int
     # approximate the gene where each deletion was identified
     ins_seqs['gene'] = ins_seqs['absolute_coords'].apply(lambda x: int(x.split(':')[0])).apply(map_gene_to_pos)
     return ins_seqs
+
 
 def process_cns_seqs(cns_data: Align.MultipleSeqAlignment, patient_zero: str,
                      start_pos: int, end_pos: int) -> (dict, str):
@@ -276,7 +377,7 @@ def remove_insertions(seq: str, positions: list) -> str:
     return seq
 
 
-def get_coords(x):
+def get_indel_coords(x):
     """helper function to get deletion coordinates (start:end) from a Pandas Series containing deletion positions"""
     min_pos = np.min(x)
     max_pos = np.max(x)
